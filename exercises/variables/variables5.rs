e1b7a98b2925187dc6fac0c0671a77a3e649012f
Junious <chengjy42@gmail.com>
2020-09-27 21:57:51 +0200

    chore: fixed test name

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -4,8 +4,15 @@
 // I AM NOT DONE
 
 fn main() {
+<<<<<<< HEAD
     let number = "3"; // don't change this line
     println!("Number {}", number);
     let number = 3;
     println!("Number {}", number);
+=======
+    let number = "T-H-R-E-E";
+    println!("Spell a Number : {}", number);
+    number = 3;
+    println!("Number plus two is : {}", number + 2);
+>>>>>>> 2ec0bdf... fix(variables5) : make shadowing more prominent
 }
