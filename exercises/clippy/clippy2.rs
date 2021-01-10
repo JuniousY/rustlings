38757cc184ed5a4158e42d8316e0caa4166c0700
Junious <chengjy42@gmail.com>
2021-01-11 00:43:30 +0800

    .

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -2,12 +2,13 @@
 // Make me compile! Execute `rustlings hint clippy2` for hints :)
 
 // I AM NOT DONE
+// 不清楚这样写是不是符合题目本意
 
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
-        res += x;
+    if let Some(12) = option {
+        res += 12;
     }
     println!("{}", res);
 }
